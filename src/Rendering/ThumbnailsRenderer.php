8d45c011d99eb04d863d1e536ec142f1b761c2d6
Dmytro Kovalenko <dmytrokovalenko.new@gmail.com>
2026-05-07 13:10:45 -0600

    feat: add SliderRenderer and ThumbnailsRenderer

---
+++ src/Rendering/ThumbnailsRenderer.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GoogleReview\Rendering;
+
+use GoogleReview\Contracts\ILayoutRenderer;
+
+final class ThumbnailsRenderer implements ILayoutRenderer
+{
+    public function __construct(
+        private readonly StarRenderer $starRenderer,
+        private readonly RelativeDateFormatter $dateFormatter,
+    ) {}
+
+    public function render(array $settings): void
+    {
+        $reviews = $this->enrichReviews($settings['list'] ?? []);
+
+        include GOOGLE_REVIEWS_PLUGIN_DIR . 'widgets/templates/thumbnails-layout.php';
+    }
+
+    /** @param array<int, array<string, mixed>> $list */
+    private function enrichReviews(array $list): array
+    {
+        return array_map(function (array $item): array {
+            $item['stars_html']    = $this->starRenderer->renderStars((float) ($item['stars'] ?? 5))
+                                   . $this->starRenderer->renderVerifiedTick();
+            $item['relative_date'] = $this->dateFormatter->format($item['date'] ?? '');
+
+            return $item;
+        }, $list);
+    }
+}
